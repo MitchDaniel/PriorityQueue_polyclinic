099873d1ddda5ba81ed0e7e9fb3516186e8d7c18
mitchdaniel <mars529440@gmail.com>
2024-07-14 20:58:57 +0300

    Resolve merge conflicts

---
+++ Patient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Patient
+    {
+        public Patient(string fullname, int age)
+        {
+            Fullname = fullname;
+            Age = age;
+        }
+        public string Fullname { set; get; }
+
+        public int Age { set; get; }
+
+        List<string> _diseases = new List<string>();
+
+        public void AddDisease(string disease) => _diseases.Add(disease);
+
+        public void RemoveDisease(string disease)
+        {
+            if(_diseases.Remove(disease))
+            {
+                throw new Exception("Disease not found");
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder patient = new StringBuilder();
+            patient.Append(Fullname + ", " + Age.ToString() + " years old\n");
+            foreach (var disease in _diseases)
+            {
+                patient.Append(disease);
+            }
+            return patient.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Xml.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var priorityQueue = new PriorityQueue<Patient, int>();
+            priorityQueue.Enqueue(new Patient("Denis", 19), 1);
+            priorityQueue.Enqueue(new Patient("Ivan", 31), 2);
+            priorityQueue.Enqueue(new Patient("Sergey", 40), 3);
+            priorityQueue.Enqueue(new Patient("Oleksey", 29), 1);
+
+            while (priorityQueue.Count > 0)
+            {
+                var patient = priorityQueue.Dequeue();
+                Console.WriteLine(patient);
+            }
+        }
+    }
+}
+
+//Создайте приложение, эмулирующее очередь в поликлинику. 
+//Пациенты приходят к врачу и попадают в очередь.
+//Если в очереди три пациента и приходит четвёртый, он попадает в конец очереди. 
+//Врач принимает по одному пациенту за определенный момент времени.
+//При разработке приложения используйте класс PriorityQueue.
